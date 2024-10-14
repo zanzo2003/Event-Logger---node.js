f2272cdbd0992d47916b50cf9f1c244426c79b67
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2024-10-14 15:23:45 +0530

    Fixed logging error, changes file format, formatted error msg

--- logger.js
+++ logger.js
@@ -3,22 +3,26 @@ const fs = require("fs")
 const os = require("os")
 
 const EventEmmiter = require('events')
-const { memoryUsage } = require("process")
 
 class Logger extends EventEmmiter{
 
   log(message){
-    this.emit("Message", {message})
+    this.emit("message", {message})
   }
 }
 
 
 const logger = new Logger()
-const logFile = './eventlog.text'
+const logFile = './eventlog.txt'
 
 const logToFile = (event)=>{
-  const logMessage = `${new Date().toISOString} - ${event.message} \n`;
-  fs.appendFileSync(logFile, logMessage)
+  const logMessage = `${new Date().toISOString()} - ${event.message} \n`;
+
+  fs.appendFile(logFile, logMessage, (error)=>{
+    if(error){
+      console.log('Failed to write to file :', error)
+    }
+  })
 }
 
 logger.on('message', logToFile)
@@ -26,7 +30,7 @@ logger.on('message', logToFile)
 setInterval(()=>{
 
   const totalMemory = (os.freemem() / os.totalmem()) * 100
-  logger.log(`Current memory useage: ${totalMemory.toFixed(2)}`)
+  logger.log(`Current memory useage: ${totalMemory.toFixed(2)}%`)
 
 }, 3000);
 
